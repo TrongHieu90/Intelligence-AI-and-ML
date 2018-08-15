c98627dcdfcb5f08143d5da9a98f280ac3ec76ca
Trong Hieu Nguyen <tronghieu.designer@gmail.com>
2018-08-15 17:21:47 +0200

    Add files via upload

---
+++ PathfindingAStar/WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Canvas = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Canvas.Location = new System.Drawing.Point(0, 0);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(800, 426);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(372, 447);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "DrawGrid";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InputBox
+            // 
+            this.InputBox.Location = new System.Drawing.Point(246, 447);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(70, 20);
+            this.InputBox.TabIndex = 2;
+            this.InputBox.TextChanged += new System.EventHandler(this.InputBox_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(517, 447);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "DrawPath";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 492);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Canvas);
+            this.Name = "Form1";
+            this.Text = "PathfindingA*";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Canvas;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PathfindingAStar/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private int rows;
+        private int cols;
+        //private Spot[][] grid; //jagged array
+        private Spot[,] grid;
+        private List<Spot> openSet = new List<Spot>();
+        private List<Spot> closedSet = new List<Spot>();
+        
+        private int c_width; //width of each spot/cell
+        private int c_height; //width of each spot/cell
+
+        Spot Start;
+        Spot End;
+        Spot Current;
+
+        public Form1()
+        {
+            InitializeComponent();          
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e) //main canvas/panel to draw
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) //draw button
+        {
+            int? input = null;
+            try
+            {
+                input = int.Parse(InputBox.Text);
+            }
+            catch (FormatException error)
+            {
+                Console.WriteLine($"FormatException: {error.Source}");
+            }
+            if (input != null)
+            {
+                rows = (int)input;
+                cols = (int)input;
+                c_width = Canvas.Width / cols;
+                c_height = Canvas.Height / rows;
+
+                Setup();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Pathfind();
+        }
+
+        private void InputBox_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        public class Spot:Form1
+        {
+            public int i;
+            public int j;
+            public int f;
+            public int g;
+            public int h;
+            public List<Spot> neighbors = new List<Spot>();
+            public Spot previous = null;
+
+            public Spot(int i, int j)
+            {
+                this.i = i;
+                this.j = j;
+               
+            }
+            
+            public void Display(SolidBrush brushColor)
+            {
+                //Console.WriteLine($"Displaying Spot: {i} and {j}");
+                Spot spot = new Spot(i, j);
+                Graphics grph = Canvas.CreateGraphics();           
+                Rectangle rect = new Rectangle(i * c_width, j * c_height, c_width, c_height);
+                grph.FillRectangle(brushColor, rect);
+
+            }
+
+
+            public void AddNeighbors()
+            {
+                int i = this.i;
+                int j = this.j;
+
+                //right adjacent spot
+                if (i < cols - 1)
+                {
+                    neighbors.Add(grid[i + 1,j]);
+                }
+
+                //left adjacent spot
+                if (i > 0)
+                {
+                    neighbors.Add(grid[i - 1,j]);
+                }
+
+                //up adjacent spot
+                if (j < rows - 1)
+                {
+                    neighbors.Add(grid[i,j + 1]);
+                }
+
+                //down adjacent spot
+                if (j > 0)
+                {
+                    neighbors.Add(grid[i,j - 1]);
+                }
+            }
+        }
+
+        private void Setup()
+        {
+            Graphics g = Canvas.CreateGraphics();
+            Pen myPen = new Pen(Brushes.Black, 1);
+
+            //2d array
+            grid = new Spot[rows, cols];
+
+            //initizaling each cell/spot in our grid
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    grid[i,j] = new Spot(i,j);
+
+                    //drawing our grid
+                    Rectangle rect = new Rectangle(i * c_width, j * c_height, c_width, c_height);
+                    g.DrawRectangle(myPen, rect);
+
+                }
+            }
+
+            //Adding neighbors
+            for (int i = 0; i < cols; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    grid[i,j].AddNeighbors();
+                }
+            }
+
+            //Set Start point and End Point and fill them with purple color
+            Start = grid[0, 0];
+            SolidBrush purpleBrush = new SolidBrush(Color.Purple);
+            Rectangle startRect = new Rectangle(0 * c_width, 0 * c_height, c_width, c_height);
+            g.FillRectangle(purpleBrush, startRect);
+
+            End = grid[rows - 1, cols - 1];
+            Rectangle endRect = new Rectangle((rows - 1) * c_width, (cols -1) * c_height, c_width, c_height);
+            g.FillRectangle(purpleBrush, endRect);
+
+            //Putting Start point as openSet's first element
+            openSet.Add(Start);
+        }
+
+        private void Pathfind()
+        {
+            while(true)
+            {              
+                if (openSet.Count > 0)
+                {
+                    int winner = 0;
+                    for(int i = 0; i < openSet.Count; i++)
+                    {
+                        if(openSet[i].f < openSet[winner].f)
+                        {
+                            winner = i;
+                        }
+                    }
+
+                    Current = openSet[winner];
+
+                    if(Current == End)
+                    {
+                        Console.WriteLine("Pathfinding done");
+                        break;
+                        
+                    }
+                    
+                    RemoveElement(openSet, Current);
+                    
+                    closedSet.Add(Current);
+                    
+                    var neighbors = Current.neighbors;                  
+
+                    
+                    //checking each neighbor Spot
+                    for (int i = 0; i < neighbors.Count; i++)
+                    {
+                        Console.WriteLine("checking neighbors");
+                        var neighbor = neighbors[i];
+                        if(!closedSet.Contains(neighbor))
+                        {
+                            var tempG = Current.g + 1;
+                            var newPath = false;
+
+                            if(openSet.Contains(neighbor))
+                            {
+                                if(tempG < neighbor.g)
+                                {
+                                    neighbor.g = tempG;
+                                    newPath = true;
+                                }
+                            }
+                            else
+                            {
+                                neighbor.g = tempG;
+                                newPath = true;
+                                openSet.Add(neighbor);
+                            }
+                            if(newPath)
+                            {
+                                neighbor.h = Heuristic(neighbor, End);
+                                neighbor.f = neighbor.g + neighbor.h;
+                                neighbor.previous = Current;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("no solution");
+                    return;
+                }
+            }
+            VisualizingPath();
+
+        }
+
+        private int Heuristic(Spot a, Spot b)
+        {
+            //Euclidean distance function
+            var dist = Math.Sqrt(((a.i - b.i) * (a.i - b.i) + (a.j - b.j) * (a.j - b.j)));
+            return (int)dist;
+        }
+
+        private void VisualizingPath()
+        {          
+            for (int i = 0; i < closedSet.Count; i++)
+            {
+                closedSet[i].Display(new SolidBrush(Color.Green));
+            }
+
+            for (int i = 0; i < closedSet.Count; i++)
+            {
+                closedSet[i].Display(new SolidBrush(Color.Red));
+            }
+
+            List<Spot> path = new List<Spot>();
+            Spot temp = Current;
+            path.Add(temp);
+
+            while(temp.previous != null)
+            {
+                path.Add(temp.previous);
+                temp = temp.previous;
+            }
+
+            for (int i = 0; i < closedSet.Count; i++)
+            {
+                path[i].Display(new SolidBrush(Color.Blue));
+            }
+        }
+        
+        private void RemoveElement(List<Spot> list, Spot element)
+        {
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                if (list[i] == element)
+                {
+                    list.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
